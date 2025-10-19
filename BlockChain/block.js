7f898b58def838416073c400d096c8bd8ba0ecc3
Felipe Selva ☠ <sellvafelipe@gmail.com>
2025-10-18 22:40:53 -0300

    Algoritimo-de-Mineração
    Criação do algoritimo de mineração e conhecimento logico de nonces

--- BlockChain/block.js
+++ BlockChain/block.js
@@ -4,18 +4,27 @@ const sha256 = require ('crypto-js/sha256');
 
 
 module.exports = class Block {
-    constructor(index = 0, previousHash = null, data = 'Genesis Block'){
+    constructor(index = 0, previousHash = null, data = 'Genesis Block', 
+        difficulty = '') {
         this.index = index;
         this.data = data;
         this.timestamp = new Date();
         this.prevHash = previousHash;
+        this.nonce = 0;
+        this.mine(difficulty);
+    
+}
 
-        this.hash = this.generateHash();
+    mine(prefix) {
+        do {
+            this.nonce++;
+            this.hash = this.generateHash();
+        } while(!this.hash.startsWith(prefix));
     }
 
     generateHash() {
         return sha256(this.index + this.previousHash  +
-             JSON.stringify(this.data) + this.timestamp).toString(); }
+             JSON.stringify(this.data) + this.timestamp + this.nonce).toString(); }
 
 
 
